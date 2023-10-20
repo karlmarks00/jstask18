f4ac8a02738e9bffe4704683bcb745de2e0f62bc
karlmarks00 <144471877+karlmarks00@users.noreply.github.com>
2023-10-20 23:45:56 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+
+   /* (1)
+let cat = {
+    catName: 'Tom',
+    catAge: 3
+};
+
+console.log('Имя кошки:' + cat.catName);
+console.log('Возраст кошки:' + cat.catAge + 'года');
+*/
+
+   /* (2)
+let car = {
+    name: 'Ford Mustang',
+    year: 1969
+}
+console.log('Марка автомобиля:' + car.name);
+console.log('Год выпуска:' + car.year);
+
+car.year = 1970;
+
+console.log('Марка автомобиля:' + car.name);
+console.log('Год выпуска (after changes):' + car.year);
+*/
+
+  /*     (3)
+let book = {
+    name: 'Цель',
+    author: 'Элияху Голдратт',
+    year: 1984
+}
+
+console.log('Название книги:' + book.name);
+console.log('Автор:' + book.author);
+console.log('Год выпуска:' + book.year);
+
+book.genre = 'Бизнес-роман'
+
+console.log('Название книги:' + book.name);
+console.log('Автор:' + book.author);
+console.log('Год выпуска:' + book.year);
+console.log('Жанр:' + book.genre);
+*/
+
+      /*  (4)
+let studet = {
+    имя: "Иван",
+    возраст: 20,
+    средний_балл: 4.5
+  }
+  
+console.log("Имя студента: " + studet.имя);
+console.log("Возраст студента: " + studet.возраст + " лет");
+console.log("Средний балл студента: " + studet.средний_балл);
+  
+studet.средний_балл = 4.8;
+  
+console.log("Средний балл студента (после изменения): " + studet.средний_балл);
+  */
+
+  (5)
+
+let fruits = {
+    name: 'яблоко',
+    color: 'красный',
+    taste: 'сладкий'
+}
+console.log('Название:' + fruits.name );
+console.log('Цвет:' + fruits.color);
+console.log('Вкус:'  + fruits.taste);
+
+fruits.number = '2кг'
+
+console.log('Название:' + fruits.name );
+console.log('Цвет:' + fruits.color);
+console.log('Вкус:' + fruits.taste);
+console.log('Вес' + fruits.number);
